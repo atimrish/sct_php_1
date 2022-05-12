a041e969e59ec9b6b3547343f62b557078c8c38f
David9310 <Davidlev27@mail.ru>
2022-05-12 18:56:46 +0400

    lesson_10

---
+++ 1.php
@@ -0,0 +1,27 @@
+<?php
+
+
+
+?>
+
+<form action="" method="get">
+
+    <label for="login">Логин</label>
+    <input type="text" name="login" id="login">
+    <br>
+    <br>
+
+    <label for="pass">Пароль</label>
+    <input type="text" name="pass" id="pass">
+    <br>
+    <br>
+
+    <label for="age">Возраст</label>
+    <input type="text" name="age" id="age">
+    <br>
+    <br>
+
+
+    <input type="submit" value="Отправить">
+
+</form>
