45e036c9fb43b456f2eb7cf5df7e7bfdccc89d02
David9310 <Davidlev27@mail.ru>
2022-05-16 19:01:03 +0400

    lesson_11

--- 1.php
+++ 1.php
@@ -1,5 +1,7 @@
 <?php
 
+$months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+
 ?>
 
 <form action="" method="post">
@@ -36,27 +38,69 @@
     <br>
 
     <label for="date">Дата рождения</label>
-    <input type="date" name="date" id="date">
+    <select name="date" id="date">
+        <?php
+
+            for ($i = 1; $i < 32; $i++) {
+                echo "<option value='$i'>$i</option>";
+            }
+
+        ?>
+    </select>
+    <br>
+
 
     <label for="month">Месяц</label>
-    <input type="month" name="month" id="month">
+    <select name="month" id="month">
+
+        <?php
+
+        foreach ($months as $key => $value)
+        {
+            echo "<option value='$value'>$value</option>";
+        }
+
+        ?>
+
+    </select>
+    <br>
+
 
     <label for="year">Год</label>
     <select name="year" id="year">
 
-        <option value="2000">2000</option>
-        <option value="2001">2001</option>
-        <option value="2002">2002</option>
-        <option value="2003">2003</option>
+        <?php
+
+        for($i = 1910; $i < 2022; $i++)
+        {
+            echo "<option value='$i'>$i</option>";
+        }
+
+
+//        if (!empty($_POST['btn']))
+//        {
+//
+//
+//
+//        }
+
+
+
+        ?>
         
     </select>
+    <br>
 
     <label for="email">Email</label>
     <input type="email" name="email" id="email">
+    <br>
+
 
     <label for="country">Гражданство</label>
     <input type="text" name="country" id="country">
+    <br>
 
-    <input type="submit" value="Отправить">
-    
+    <input type="submit" value="Отправить" name="btn">
+    <br>
+
 </form>
