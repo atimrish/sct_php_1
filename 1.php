db9e0c7b89cd3490b46709a9871272fdeb81451b
David9310 <Davidlev27@mail.ru>
2022-05-19 17:28:32 +0400

    lesson_11

--- 1.php
+++ 1.php
@@ -2,27 +2,63 @@
 
 $months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
 
+if (!empty($_POST['btn'])) :
+
+
 ?>
+
+    <style>
+
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid black;
+            padding: 10px;
+        }
+
+    </style>
+
+<table>
+    <tr><td>Фамилия</td><td>Имя</td><td>Отчество</td><td>Пол</td><td>Место рождения</td>
+        <td>Дата рождения</td><td>Месяц</td><td>Год</td><td>Email</td><td>Гражданство</td></tr>
+    <tr>
+        <td><?= $_POST['surname'] ?></td>
+        <td><?= $_POST['name'] ?></td>
+        <td><?= $_POST['lastname'] ?></td>
+        <td><?= $_POST['gender'] ?></td>
+        <td><?= $_POST['place'] ?></td>
+        <td><?= $_POST['date'] ?></td>
+        <td><?= $_POST['month'] ?></td>
+        <td><?= $_POST['year'] ?></td>
+        <td><?= $_POST['email'] ?></td>
+        <td><?= $_POST['country'] ?></td>
+    </tr>
+
+</table>
+
+<?php endif; ?>
 
 <form action="" method="post">
 
-    <label for="surname">Фпмилия</label>
-    <input type="text" name="surname" id="surname">
+    <label for="surname">Фамилия</label>
+    <input type="text" name="surname" id="surname" required>
     <br>
     
     <label for="name">Имя</label>
-    <input type="text" name="name" id="name">
+    <input type="text" name="name" id="name" required>
     <br>
     
     <label for="lastname">Отчество</label>
-    <input type="text" name="lastname" id="lastname">
+    <input type="text" name="lastname" id="lastname" required>
     <br>
 
     <label for="gender">Пол</label>
     <select name="gender" id="gender">
 
-        <option value="man">Мужской</option>
-        <option value="woman">Женский</option>
+        <option value="Мужской">Мужской</option>
+        <option value="Женский">Женский</option>
         
     </select>
     <br>
@@ -76,28 +112,22 @@ $months = ['январь', 'февраль', 'март', 'апрель', 'май
             echo "<option value='$i'>$i</option>";
         }
 
-
-//        if (!empty($_POST['btn']))
-//        {
-//
-//
-//
-//        }
-
-
-
         ?>
         
     </select>
     <br>
 
     <label for="email">Email</label>
-    <input type="email" name="email" id="email">
+    <input type="email" name="email" id="email" required>
     <br>
 
 
     <label for="country">Гражданство</label>
-    <input type="text" name="country" id="country">
+    <select name="country" id="country">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+    </select>
     <br>
 
     <input type="submit" value="Отправить" name="btn">
